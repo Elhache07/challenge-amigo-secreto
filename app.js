26c194f4e5d6eaf0c0ac43d20db529aa501cf00d
elhache07 <manuelhache723@gmail.com>
2025-06-25 17:24:05 -0500

    Actualizando archivos

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
+
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-=======
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
->>>>>>> 4e7c1c8 (Agregando archivos)

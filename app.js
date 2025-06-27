7c8ceb041ef97d8b85707fa2ff91c6fb424d42b1
elhache07 <manuelhache723@gmail.com>
2025-06-27 16:42:11 -0500

    implementando lista y sorteo

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
+let amigo= [];
 
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo(){
+    //Captura el valor del input
+    const teclado=document.getElementById("amigo");
+    const nombre=teclado.value.trim();// eliminamos espacios innecesarios
+
+    if(nombre== ""){
+        alert("Por favor, inserte un nombre");
+        return
+    }
+
+    amigo.push(nombre)
+
+    const lista=document.getElementById("listaAmigos");
+
+    const item= document.createElement("li");
+
+    item.textContent = nombre;
+
+    lista.appendChild(item);
+
+
+    teclado.value="";
+
+    
+
+}
+
+function sortearAmigo(){
+
+      const resultado = document.getElementById("resultado");
+
+      if (amigo.length === 0) {
+        resultado.innerHTML = "No hay amigos para sortear.";
+        return;
+      }
+
+      const indiceAleatorio = Math.floor(Math.random() * amigo.length);
+      const amigoSorteado = amigo[indiceAleatorio];
+
+      resultado.innerHTML = "El amigo sorteado es: " + amigoSorteado;
+    
+
+}
